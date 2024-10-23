3e35855487ec92d63ac61aba3c0248d5e93c0bb2
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-10-23 17:47:27 +0200

    Created condition with allert for start button

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,17 @@
 /*Creating a grid 8x8 */
-/*  Creo una costate che selezioni il tag in cui inserire la griglia
-    */
 
 const start = document.getElementById("start").addEventListener("click", createGrid);
 const grid = document.getElementsByTagName("section")[0];
 
 function createGrid (){
-    // if(){
+    if(grid.hasChildNodes()){
+        alert('La partita é gia stata avviata');
+    }
+    else{
         for (let index = 0; index < 17; index++) {
             console.log('Cell' + index);
             let createCell = document.createElement("div");
             grid.appendChild(createCell);
         };
-    // }
-    // else{
-    //     prompt('La partita é gia stata avviata');
-    // }
+    }
 };
