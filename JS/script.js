d5f14e2caf616d77f29323e95c8b7897892bd784
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-10-24 14:32:43 +0200

    Added pieces on grid

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,10 @@
 /*Creating a grid 8x8 */
+    const blackPieces = [
+    'Rook-B', 'Knight-B', 'Bishop-B', 'Queen-B', 'King-B', 'Bishop-B', 'Knight-B', 'Rook-B','Pawn-B', 'Pawn-B', 'Pawn-B', 'Pawn-B', 'Pawn-B', 'Pawn-B', 'Pawn-B', 'Pawn-B'              
+    ];
+    const whitePieces = [
+    'Pawn-W', 'Pawn-W', 'Pawn-W', 'Pawn-W', 'Pawn-W', 'Pawn-W', 'Pawn-W', 'Pawn-W','Rook-W', 'Knight-W', 'Bishop-W', 'Queen-W', 'King-W', 'Bishop-W', 'Knight-W', 'Rook-W'     
+    ];
 
 const start = document.getElementById("start").addEventListener("click", createGrid);
 const grid = document.getElementsByTagName("section")[0];
@@ -12,11 +18,23 @@ function createGrid (){
             console.log('Cell' + index);
             let createCell = document.createElement("div");
             grid.appendChild(createCell);
+            if(index < 16){
+                let img = document.createElement("img");
+                img.src = "img/" + blackPieces[index] + ".png";
+                img.alt = blackPieces[index];
+                createCell.appendChild(img);
+            };
         };
         for (let index = 0; index < 32; index++) {
             console.log('Cell' + index);
             let createCell = document.createElement("div");
             grid.appendChild(createCell);
+            if(index > 15){
+                let img = document.createElement("img");
+                img.src = "img/" + whitePieces[index - 16] + ".png";
+                img.alt = whitePieces[index - 16];
+                createCell.appendChild(img);
+            };
         };
     };
 };
